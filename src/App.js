15e6ab4f06d8860773bf40077027366412f6011e
FeliOdras <ariane.feldmann@odras.de>
2019-06-12 14:32:54 +0200

    Created constant for needed values

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import "./App.css";
 import { connect } from "react-redux";
 import { API_CALL_REQUEST } from "./actions/actionTypes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { fetching, numberTrivia, onRequestNumber, error } = this.props;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
