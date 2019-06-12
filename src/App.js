b3051ea3ea60f0b0d5574055ff2fe4b7b785847d
FeliOdras <ariane.feldmann@odras.de>
2019-06-12 14:29:05 +0200

    Import redux connect and action to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { connect } from "react-redux";
+import { API_CALL_REQUEST } from "./actions/actionTypes";
 
 function App() {
   return (
