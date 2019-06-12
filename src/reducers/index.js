1d8445be226e7b4e5ffee3c7c8891f01288dedce
FeliOdras <ariane.feldmann@odras.de>
2019-06-12 13:43:37 +0200

    Created Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,3 +9,16 @@ const initialState = {
   number: 23,
   error: null
 };
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case API_CALL_REQUEST:
+      return { ...state, fetching: true, error: null };
+    case API_CALL_SUCCESS:
+      return { ...state, fetching: false };
+    case API_CALL_FAILURE:
+      return { ...state, fetching: false };
+    default:
+      return state;
+  }
+}
