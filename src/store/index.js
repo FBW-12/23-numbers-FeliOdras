9f64bdf576015eebf314644b4b80409f0df8cf3a
FeliOdras <ariane.feldmann@odras.de>
2019-06-12 14:18:57 +0200

    Created Saga Middleware and implemented redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import createSagaMidleware from "redux-saga";
+import createSagaMiddleware from "redux-saga";
 import { watcherSaga } from "../sagas";
 import { reducer } from "../reducers";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
