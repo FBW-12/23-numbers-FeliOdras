91e8767dfd0977e06dbdfd5d1d92619ac2aa9501
FeliOdras <ariane.feldmann@odras.de>
2019-06-12 13:40:05 +0200

    Imported action tyopes and set initial state

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import {
+  API_CALL_REQUEST,
+  API_CALL_SUCCESS,
+  API_CALL_FAILURE
+} from "../actions/actionTypes";
+
+const initialState = {
+  fetching: false,
+  number: 23,
+  error: null
+};
