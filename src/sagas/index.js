0fa1d81acc9769db67e44b38b25071b721554d55
FeliOdras <ariane.feldmann@odras.de>
2019-06-12 13:52:46 +0200

    Added fetch with static number

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -5,3 +5,14 @@ import {
   API_CALL_FAILURE
 } from "../actions/actionTypes";
 import axios from "axios";
+
+export function* watcherSaga() {
+  yield takeLatest(API_CALL_REQUEST, workerSaga);
+}
+
+function* fetchNumber() {
+  return axios({
+    method: "get",
+    url: "https://numbers-api-proxy.dci-fbw121.now.sh/?number=23"
+  });
+}
